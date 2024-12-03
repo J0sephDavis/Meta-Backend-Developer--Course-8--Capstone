466d82feb77f54d6bacad3d4b54bb3e29676ed75
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-12-03 13:27:14 -0500

    get_item functon for Menu ORM which returns a string

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -12,3 +12,6 @@ class Menu(models.Model):
     title = models.CharField(max_length=255);
     price = models.DecimalField(max_digits=10, decimal_places=2); # I assume thats what the 10,2 meant in the screenshot in "Module 2 > Exercise: Settings up the models";
     inventory = models.IntegerField(); # max length might have been intended to be 5. But from the screenshot they sent its unclear.
+    
+    def get_item(self):
+        return f"{self.title} : {str(self.price)}";
