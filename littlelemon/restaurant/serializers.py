7625c8653d10ba0fc664727868ed1786b74f70c4
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-12-03 13:29:10 -0500

    pedantic

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -5,10 +5,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User;
         fields = ['url', 'username', 'email', 'groups'];
+
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu;
         fields = "__all__";
+
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking;
