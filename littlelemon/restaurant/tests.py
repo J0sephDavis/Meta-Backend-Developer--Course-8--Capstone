080cde1d2182fd3402e6a02add56e57bbe868dff
J0sephDavis <38631734+J0sephDavis@users.noreply.github.com>
2024-12-03 13:27:24 -0500

    test the get_item function using unit tests

--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Test Models
+from .models import Menu;
+class MenuTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title="IceCream", price=80, inventory=100);
+        item_str = item.get_item();
+        self.assertEqual(item_str, "IceCream (100) @ $80");
